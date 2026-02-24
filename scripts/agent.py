07ecb099a043047a3e365cfbdad2a8afbf759868
Esteban Peregrina <110783029+esteban-peregrina@users.noreply.github.com>
2026-02-24 15:05:27 +0100

    FEAT: Rename file and add SARSA learning

--- scripts/agent.py
+++ scripts/agent.py
@@ -16,6 +16,8 @@ class Easy21Agent:
         
         self.actions = ['hit', 'stick']
 
+        self.Qstar = np.zeros((10, 21, 2)) # Optimal quality matrix, store optimal action score for each state
+
     def get_action(self, state):
         """
         Compute agent action using epsilon-greedy strategy
@@ -34,7 +36,7 @@ class Easy21Agent:
             index = np.argmax(self.Q[dealer_card_index, player_sum_index, :]) # Look for the best action to do in this state (from experience)
             return self.actions[index] # Exploit best action 
 
-    def learn(self, episodes = 500000):
+    def mc_learn(self, episodes = 500000):
         """
         Implement Monte Carlo epsilon-greedy control
         
@@ -58,8 +60,61 @@ class Easy21Agent:
                 
                 # Update quality matrix
                 alpha = 1.0 / self.N[dealer_card_index, player_sum_index, action_index] 
-                error = reward - self.Q[dealer_card_index, player_sum_index, action_index] # Was the reward actually better than excpected, or worse ?
+                error = reward - self.Q[dealer_card_index, player_sum_index, action_index] # Was the LAST reward actually better than excpected, or worse ?
                 self.Q[dealer_card_index, player_sum_index, action_index] += alpha * error # Correct the action's score for this state, with less and less weight as experience increase
+
+        self.Qstar = np.copy(self.Q) # After learning, we can consider that the learnt quality matrix is the optimal one
+
+    def sarsa_learn(self, lambd4, episodes = 1000):
+        """
+        Implement SARSA epsilon-greey controle
+        
+        :param episodes: Number of game the agent will play to train itself
+        """
+        for _ in range(episodes):
+            current_state = init_game() # Get the t=0 state
+            terminal = False
+            current_action = self.get_action(current_state) # Predict the t=0 action
+
+            # Eligibility traces matrix, same size (10, 21, 2) as quality matrix
+            # Stores how much each state-action pair is relevant (eligible) for the current state
+            E = np.zeros_like(self.Q) 
+            # Play the game  using the learnt quality matrix and update it at each step
+            while not terminal:
+                # Current instant t
+                d_idx = current_state['dealer'] - 1 # Dealer first card index, drawn at game's start
+                p_idx = current_state['player'] - 1 # Player sum index
+                a_idx = 0 if current_action == 'hit' else 1 # Action index
+
+                self.N[d_idx, p_idx, a_idx] += 1 # Mark this scenario as being visited one more time
+                E[d_idx, p_idx, a_idx] += 1 # Mark this scenario as being visited one more time
+                
+                # Next instant t+1
+                next_state, next_reward, terminal = step(current_state, current_action) # Executing the t action lead to the t+1 reward and the t+1 state
+
+                # Compute t time difference error (between t+1 and t !)
+                if not terminal:
+                    next_action = self.get_action(next_state) # Predict the t+1 action
+                    nd_idx = next_state['dealer'] - 1
+                    np_idx = next_state['player'] - 1
+                    na_idx = 0 if next_action == 'hit' else 1
+                    
+                    delta = next_reward + self.Q[nd_idx, np_idx, na_idx] - self.Q[d_idx, p_idx, a_idx] # t time-difference error
+                else:
+                    delta = next_reward - self.Q[d_idx, p_idx, a_idx] 
+                    next_action = None # End of the game
+                
+                # Update quality matrix on the go
+                alpha = 1.0 / self.N[d_idx, p_idx, a_idx]
+                self.Q += alpha * delta * E # Correct the action's score for all state, with less and less weight as experience increase (alpha), and with less and less weights as we go futher from the current state (E and so lambda)
+                
+                # Reduce trace eligibility for all state-action pair
+                E *= lambd4
+
+                # Iterating : t becomes t+1
+                if not terminal:
+                    current_state = next_state
+                    current_action = next_action
 
 def plot_value_function(agent, title="Optimal Value Function"):
     """
@@ -111,7 +166,7 @@ def plot_optimal_policy(agent, title="Optimal Policy"):
 
 if __name__ == '__main__':
     agent = Easy21Agent()
-    agent.learn() 
+    agent.sarsa_learn(0.1) 
     plot_value_function(agent)
     plot_optimal_policy(agent)
 
